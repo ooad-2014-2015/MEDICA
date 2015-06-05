212493e2001f82e467f04e6ac5d83b5286fbe70f
pdeni <denipencl@gmail.com>
2015-06-05 10:27:36 +0200

    MobApp

---
+++ Mobile App/Main App/MEDICAAppointments/MEDICAAppointments/MainPage.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MEDICAAppointments.Resources;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using Microsoft.Phone.Data.Linq;
+using Microsoft.Phone.Data.Linq.Mapping;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MEDICAAppointments
+{
+    public partial class MainPage : PhoneApplicationPage, INotifyPropertyChanged
+    {
+        private ToDoDataContext toDoDB;
+
+        private ObservableCollection<ToDoItem> _toDoItems;
+        public ObservableCollection<ToDoItem> ToDoItems
+        {
+            get
+            {
+                return _toDoItems;
+            }
+            set
+            {
+                if (_toDoItems != value)
+                {
+                    _toDoItems = value;
+                    NotifyPropertyChanged("ToDoItems");
+                }
+            }
+        }
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            // Define query to gather all of the to-do items.
+            var toDoItemsInDB = from ToDoItem todo in toDoDB.ToDoItems
+
+                                select todo;
+
+            // Execute query and place results into a collection.
+            ToDoItems = new ObservableCollection<ToDoItem>(toDoItemsInDB);
+
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class ToDoDataContext : DataContext
+    {
+        // Specify the connection string as a static, used in main page and app.xaml.
+        public static string DBConnectionString = "Data Source=isostore:/ToDo.sdf";
+
+        // Pass the connection string to the base class.
+        public ToDoDataContext(string connectionString) : base(connectionString) { }
+
+        // Specify a single table for the to-do items.
+        public Table<ToDoItem> ToDoItems;
+    }
+
+    [Table]
+    public class ToDoItem : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        // Define ID: private field, public property and database column.
+        private int _toDoItemId;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+        public int ToDoItemId
+        {
+            get
+            {
+                return _toDoItemId;
+            }
+            set
+            {
+                if (_toDoItemId != value)
+                {
+                    NotifyPropertyChanging("ToDoItemId");
+                    _toDoItemId = value;
+                    NotifyPropertyChanged("ToDoItemId");
+                }
+            }
+        }
+
+        // Define item name: private field, public property and database column.
+        private string _itemName;
+
+        [Column]
+        public string ItemName
+        {
+            get
+            {
+                return _itemName;
+            }
+            set
+            {
+                if (_itemName != value)
+                {
+                    NotifyPropertyChanging("ItemName");
+                    _itemName = value;
+                    NotifyPropertyChanged("ItemName");
+                }
+            }
+        }
+
+        // Define completion value: private field, public property and database column.
+        private bool _isComplete;
+
+        [Column]
+        public bool IsComplete
+        {
+            get
+            {
+                return _isComplete;
+            }
+            set
+            {
+                if (_isComplete != value)
+                {
+                    NotifyPropertyChanging("IsComplete");
+                    _isComplete = value;
+                    NotifyPropertyChanged("IsComplete");
+                }
+            }
+        }
+        // Version column aids update performance.
+        [Column(IsVersion = true)]
+        private Binary _version;
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify the page that a data context property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify the data context that a data context property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
